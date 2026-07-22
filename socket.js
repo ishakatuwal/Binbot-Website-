0849ab355c4c3c20d75eedb0b11e5c33a6f8a51d
ishakatuwal <ishakatuwal2061@gmail.com>
2026-07-22 20:00:30 +1000

    Add files via upload

---
+++ socket.js
@@ -0,0 +1,18 @@
+const { Server } = require('socket.io');
+let io;
+
+module.exports = {
+  init: (httpServer) => {
+    io = new Server(httpServer, {
+      cors: { origin: '*', methods: ['GET', 'POST'] }
+    });
+    io.on('connection', (socket) => {
+      console.log(`🔌 Client connected to Socket.io: ID ${socket.id}`);
+    });
+    return io;
+  },
+  getIO: () => {
+    if (!io) throw new Error('Socket.io not initialized!');
+    return io;
+  }
+};
