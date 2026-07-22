d5ddbe8d6bf846af4f0b427c9f0d642ee22de20f
ishakatuwal <ishakatuwal2061@gmail.com>
2026-07-22 20:18:38 +1000

    Delete routes directory

--- routes/binRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { registerBin, getAllBins, updateBinFromESP32 } = require('../controllers/binController');
-
-router.post('/register', registerBin);
-router.get('/', getAllBins);
-router.post('/esp32-update', updateBinFromESP32);
-
-module.exports = router;
