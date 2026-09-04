abe01185afe0014d77c610a1bc435a095de65142
ishakatuwal <ishakatuwal2061@gmail.com>
2026-09-04 22:50:06 +1000

    Add files via upload

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { assignTask, getTasks } = require('../controllers/taskController');
+const { assignTask, getTasks, completeTask } = require('../controllers/taskController');
 
 router.post('/assign', assignTask);
+router.post('/complete', completeTask);
+router.post('/:taskId/complete', completeTask);
 router.get('/', getTasks);
 
 module.exports = router;
